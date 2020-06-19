50238e66e9567f854a36590c9f79f8bf27033f10
colw <colw@outlook.com>
2020-06-19 03:38:20 +0200

    Added submit button and submission sentence

--- src/App.js
+++ src/App.js
@@ -8,17 +8,35 @@ const fruitOptions = [...new Set(fruits)];
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [fruit, setFruit] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setFruit(inputText);
+  }
+
+  function handleChange(event) {
+    setInputText(event.target.value);
+    setFruit("");
+  }
+
   return (
     <div className="App">
       <h1>AutoCompleteInput</h1>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <AutoCompleteInput
           value={inputText}
           initOptions={fruitOptions}
-          onChange={(event) => setInputText(event.target.value)}
+          onChange={handleChange}
           placeholder="Select a fruit…"
         />
+        <button type="submit">Submit</button>
       </form>
+      {fruit && (
+        <p>
+          You have selected: <b>{fruit}</b>!
+        </p>
+      )}
     </div>
   );
 }
